a91653ff0f26060831939324013b9b9d8b7886cf
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2021-11-10 15:56:32 +0700

    task pertemuan 4

--- Pertemuan 3/class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-#membuat class
-
-class Person
-{
-    
-}
--- Pertemuan 3/variable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-#membuat variabel
-$nama = "Fani Adi Frihandoko";
-$Jurusan = "Teknik Informatika";
-
-#mengakses variabel
-echo "$nama <br>";
-echo "$Jurusan";
