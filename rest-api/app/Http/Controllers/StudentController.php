b3b52eb2f557cbd3bf8b7da9b6820c62289d95cb
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2021-11-16 14:44:37 +0700

    task pertemuan 6

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -5,11 +5,108 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Student;
 
-
 class StudentController extends Controller
 {
-    function index(){
-        $students = Student::all();
-        echo $students;
+    // membuat function index
+    public function index() {
+
+        // menggunakan model student untuk select data
+        $student = Student::all();
+
+        $data = [
+            'message' => 'Get all student',
+            'data' => $student
+        ];
+
+        // mengirim data (json) dan kode 200
+        return response()->json($data, 200);
+    }
+
+    // membuat function store
+    function store(Request $request)
+    {
+        $student = Student::create ([
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ]);
+
+        $data = [
+            'message' => 'Student is created',
+            'data' => $student
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    // membuat function show
+    function show($id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+            $data = [
+                'message' => 'Get Detail Students',
+                'data' => $student
+            ];
+
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Data not Found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->update([
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->email
+            ]);
+
+            $data = [
+                'message' => 'Data is Updated',
+                'data' => $student
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Data not Found'
+            ];
+
+            return response()->json($data, 404);
+        }
+    }
+
+    function destroy($id)
+    {
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->delete();
+
+            $data = [
+                'message' => 'Student is Deleted'
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Data not Found'
+            ];
+
+            return response()->json($data, 404);
+        }
     }
 }
--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 // Import Animal Controller
 use App\Http\Controllers\AnimalController;
+// Import Student Controller
 use App\Http\Controllers\StudentController;
 
 /*
@@ -29,3 +30,20 @@ Route::post('/animals', [AnimalController::class, 'store']);
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+// Routing untuk Students
+
+// Get All Resource
+Route::get("/students", [StudentController::class, 'index']);
+
+// Add resources
+Route::post("/students", [StudentController::class, 'store']);
+
+// Get detail
+Route::get("/students/{id}", [StudentController::class, 'show']);
+
+// Update resource
+Route::put("/students/{id}", [StudentController::class, 'update']);
+
+// Delete resource
+Route::delete("/students/{id}", [StudentController::class, 'destroy']);
